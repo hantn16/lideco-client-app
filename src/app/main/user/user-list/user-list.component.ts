ba7bbc2bf8d100889cdeff482904c8f20997dc18
Han Trinh <hantn16@gmail.com>
2018-06-21 18:25:50 +0700

    Fix user service

--- src/app/main/user/user-list/user-list.component.ts
+++ src/app/main/user/user-list/user-list.component.ts
@@ -1,31 +1,43 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit, NgModule, OnDestroy } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { UserService } from '../user.service';
 import { User } from '../../../core/domain/user.model';
+import * as $ from 'jquery';
+import { Event } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
+
   displayDialog: boolean;
   cols: any[];
   users: User[];
+  isLoading = false;
   user: User;
   selectedUser: User;
   newUser: Boolean = true;
+  private usersSub: Subscription;
   constructor(private _userService: UserService) { }
 
   ngOnInit() {
-    this.users = this._userService.getListUsers();
-
+    // this.users = this._userService.getListUsers();
+    this.isLoading = true;
     this.cols = [
       { field: 'imgLink', header: 'Ảnh đại diện' },
       { field: 'email', header: 'Email' },
       { field: 'name', header: 'Tên' },
       { field: 'fullName', header: 'Tên đầy đủ' }
     ];
+    this._userService.getListUsers();
+    this.usersSub = this._userService.getUserUpdateListener()
+      .subscribe((users) => {
+        this.isLoading = false;
+        this.users = users;
+      });
   }
   showDialogToAdd() {
     this.newUser = true;
@@ -49,5 +61,9 @@ export class UserListComponent implements OnInit {
     user['imgLink'] = us.imgLink;
     return <User>user;
   }
-
+  resetDialog(event: Event) {
+  }
+  ngOnDestroy(): void {
+    this.usersSub.unsubscribe();
+  }
 }
--- src/app/main/user/user.service.ts
+++ src/app/main/user/user.service.ts
@@ -2,28 +2,71 @@ import { Injectable } from '@angular/core';
 import { DataService } from '../../core/services/data.service';
 import { map } from 'rxjs/operators';
 import { User } from '../../core/domain/user.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  private users: User[] = [];
+  private usersUpdated = new Subject<User[]>();
   constructor(private _dataService: DataService) { }
-  getListUsers = (): User[] => {
-    const convertedUsers = [] as User[];
-    this._dataService.get('users/getall').subscribe((res) => {
-      const kq = res.users;
-      for (const user of kq) {
-        convertedUsers.push(this.convert2UserModel(user));
-      }
+  getListUsers = () => {
+    this._dataService.get('users/getall').pipe(map(data => {
+      return data.users.map(user => {
+        return this.convert2UserModel(user);
+      });
+    })).subscribe(transformedUsers => {
+      this.users = transformedUsers;
+      this.usersUpdated.next([...this.users]);
     });
-    return convertedUsers;
   }
-  getUserById = (id: string): User => {
-    let user;
-    this._dataService.get(`user/${id}`)
-      .pipe(map((res) => user = this.convert2UserModel(res)));
-    return user;
+  getUserUpdateListener() {
+    return this.usersUpdated.asObservable();
+  }
+  getUserById = (id: string) => {
+    return this._dataService.get(`user/${id}`);
+  }
+  addUser(email: string, name: string, fullName: string, avatar: File) {
+    const userData = new FormData();
+    userData.append('email', email);
+    userData.append('name', name);
+    userData.append('fullName', fullName);
+    userData.append('avatar', avatar, email);
+    this._dataService.post(`users`, userData)
+      .subscribe(responseData => {
+        const user: User = this.convert2UserModel(responseData.user);
+        this.users.push(user);
+        this.usersUpdated.next([...this.users]);
+      });
+  }
+  updateUser(id: string, email: string, name: string, fullName: string, avatar: File | string) {
+    let userData: User | FormData;
+    if (typeof avatar === 'object') {
+      userData = new FormData();
+      userData.append('id', id);
+      userData.append('email', email);
+      userData.append('name', name);
+      userData.append('fullName', fullName);
+      userData.append('avatar', avatar, email);
+    } else {
+      userData = {
+        id: id,
+        email: email,
+        name: name,
+        fullName: fullName,
+        imgLink: avatar
+      };
+    }
+    this._dataService.patch(`users/${id}`, userData)
+      .subscribe(response => {
+        const updatedUsers = [...this.users];
+        const oldUserIndex = updatedUsers.findIndex(p => p.id === id);
+        const user: User = this.convert2UserModel(response.user);
+        updatedUsers[oldUserIndex] = user;
+        this.users = updatedUsers;
+        this.usersUpdated.next([...this.users]);
+      });
   }
   convert2UserModel = (user): User => {
     return {
